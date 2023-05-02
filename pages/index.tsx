8a6546a0122d62b1a18f267a0b7ebd643bcbddb6
hitalo <hsousa@bestprojects.com.br>
2023-05-02 00:46:42 -0300

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,110 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-  Center,
-  useColorMode,
-  Tooltip,
-  IconButton,
-  SunIcon,
-  MoonIcon,
-  Image,
-  HStack,
-  Text,
-  Heading,
   Box,
-  Link,
-  VStack,
   Button,
-  AspectRatio,
+  Heading,
+  Pressable,
+  ScrollView,
+  Text,
 } from "native-base";
+
+interface IProduct {
+  name: string
+  description: string
+  value: number
+}
+
+const hamburger: IProduct[] = [
+  {
+    name: 'X-Tudo',
+    description: 'Descricao',
+    value: 20
+  },
+  {
+    name: 'X-Burger',
+    description: 'Descricao',
+    value: 12
+  },
+  {
+    name: 'X-Bacon',
+    description: 'Descricao',
+    value: 15
+  },
+  {
+    name: 'X-Salada',
+    description: 'Descricao',
+    value: 10
+  }
+]
 
 // Start editing here, save and see your changes.
 export default function App() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  const handleSelect = (product: IProduct) => {
+    const list = [...products, product]
+    setProducts(list);
+  }
+
+  const handleCompleteOrder = () => {
+    
+  }
+
+  const total = () => {
+    return products.reduce((acc, cur) => {
+      return acc + cur.value
+    }, 0);
+  }
+
   return (
-    <Center
-      flex={1}
-      _dark={{ bg: "blueGray.900" }}
-      _light={{ bg: "blueGray.50" }}
-    >
-      <VStack alignItems="center" space="md">
-        <HStack alignItems="center" space="2xl">
-          <AspectRatio w={24} ratio={1.66}>
-            <Image
-              source={{ uri: "images/nextjs-logo.png" }}
-              alt="NextJS Logo"
-              resizeMode="contain"
-            />
-          </AspectRatio>
-          <Text fontSize="4xl">+</Text>
-          <Image
-            source={{ uri: "images/nativebase-logo.svg" }}
-            alt="NativeBase Logo"
-            size={24}
-            resizeMode="contain"
-          />
-        </HStack>
-        <Heading>Welcome to NativeBase</Heading>
-        <Text>
-          Edit{" "}
-          <Box
-            _text={{
-              fontFamily: "monospace",
-              fontSize: "sm",
-            }}
-            px={2}
-            py={1}
-            _dark={{ bg: "blueGray.800" }}
-            _light={{ bg: "blueGray.200" }}
-          >
-            src/pages/index.js
-          </Box>{" "}
-          and save to reload.
-        </Text>
-        <HStack alignItems="center" space="sm">
-          <Link href="https://nextjs.org/docs/getting-started" isExternal>
-            <Text
-              _light={{ color: "gray.700" }}
-              _dark={{ color: "gray.400" }}
-              underline
-              fontSize={"xl"}
-            >
-              Learn NextJS
-            </Text>
-          </Link>
-          <Text>/</Text>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-        </HStack>
-      </VStack>
-      <ColorModeSwitch />
-      <Link mt="6" href="https://docs.nativebase.io" isExternal>
-        <Button variant="outline" colorScheme="coolGray">
-          View Repo
-        </Button>
-      </Link>
-    </Center>
+    <Box backgroundColor="white" flex="1">
+      <Box p="4" backgroundColor="blue.500">
+        <Box flexDirection="row" justifyContent="space-between">
+          <Heading size="md" color="white">Mega Massa</Heading>
+          <Text color="white">Hamburger</Text>
+          <Text color="white">Bebidas</Text>
+          <Text color="white">Endereço</Text>
+          <Text color="white">Finalizar</Text>
+        </Box>
+      </Box>
+      <Box p="4" flex="1">
+        <ScrollView horizontal={false}>
+          {hamburger.map(item => (
+            <Pressable p="4" mb="4" backgroundColor="gray.200" alignContent="center" onPress={() => handleSelect(item)}>
+              <Text>{item.name}</Text>
+              <Text>{item.description}</Text>
+              <Text>{item.value}</Text>
+            </Pressable>
+          ))}
+        </ScrollView>
+        <Box flex="1" flexDirection="row" justifyContent="space-evenly">
+          <Box>
+            {products.map(item => (
+              <Box p="2" flexDirection="row">
+                <Text>{item.name} - </Text>
+                <Text>R$ {item.value}</Text>
+              </Box>
+            ))}
+          </Box>
+          <Box>
+            <Text>Finalize seu pedido</Text>
+            <Text>Total: {total()}</Text>
+            <a href={`https://api.whatsapp.com/send?phone=%99981965118&text=valor&text=${total()}`}>Finalizar</a>
+          </Box>
+        </Box>
+      </Box>
+      <Box>
+        Footer
+      </Box>
+    </Box>
   );
 }
-// Color Switch Component
-function ColorModeSwitch() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <Tooltip
-      label={colorMode === "dark" ? "Enable light mode" : "Enable dark mode"}
-      placement="bottom right"
-      openDelay={300}
-      closeOnClick={false}
-    >
-      <IconButton
-        position="absolute"
-        top={12}
-        right={8}
-        onPress={toggleColorMode}
-        icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
-        accessibilityLabel="Color Mode Switch"
-      />
-    </Tooltip>
-  );
-}
+
